88a4ccdbd588980e97b4bfe2c53c718f4d95e485
Eunbyul <las4134@gmail.com>
2023-01-27 07:08:19 +0000

    Add toolbar section

--- app.js
+++ app.js
@@ -1,35 +1,9 @@
-window.addEventListener("load", function() {
-  const canvas = document.querySelector("#canvas");
-  const ctx = canvas.getContext("2d");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 
-  canvas.height = window.innerHeight;
-  canvas.width = window.innerWidth;
+const drawing = (e) => {
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+}
 
-  //only draw when draw is true;
-  let draw = false;
-
-  //when mouse down, draw starts
-  canvas.addEventListener('mousedown', function() {
-    draw = true;
-  });
-
-  //when mouse is moving, then drawing
-  canvas.addEventListener('mousemove', function(e) {
-    //if draw is not true we won't draw
-    if(!draw) return;
-
-    ctx.lineWidth = 10;
-    ctx.lineCap = "round";
-
-    ctx.lineTo(e.clientX, e.clientY);
-    ctx.stroke();
-
-  })
-
-  //when mouse up, draw stop
-  canvas.addEventListener('mouseup', function() {
-    draw = false;
-  });
-
-
-})
+canvas.addEventListener("mousemove", drawing);

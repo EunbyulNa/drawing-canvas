719c62c25b8f7f3e8884827b54782ae89668a182
Eunbyul <las4134@gmail.com>
2023-01-28 15:58:25 +0000

    add sizeslide,option function

--- app.js
+++ app.js
@@ -1,9 +1,63 @@
-const canvas = document.querySelector("canvas");
+const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
+const sizeSlide = document.querySelector("#size-slider");
+const options = document.querySelectorAll(".option");
 
-const drawing = (e) => {
-  ctx.lineTo(e.offsetX, e.offsetY);
-  ctx.stroke();
+let isDrawing = false;
+let brushWidth = 5;
+let selectTool = "brush";
+
+window.addEventListener("load", function () {
+  canvas.width = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
+})
+
+function drawStart() {
+ isDrawing = true;
+ ctx.beginPath();
+ ctx.lineWidth = brushWidth;
+
 }
 
+function drawing(e) {
+  if(!isDrawing) return;
+
+
+  if(selectTool === "brush"){
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    ctx.strokeStyle ="black"
+  }else{
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+   ctx.strokeStyle ="white"
+  }
+}
+
+function drawEnd() {
+  isDrawing = false;
+}
+
+sizeSlide.addEventListener("change", function() {
+  brushWidth = sizeSlide.value;
+})
+
+options.forEach( (option) => {
+  option.addEventListener("click", function () {
+      selectTool = option.id;
+    if(  selectTool === "brush"){
+      brush.classList.add("active");
+      eraser.classList.remove("active");
+
+    }else{
+      brush.classList.remove("active");
+      eraser.classList.add("active");
+    }
+
+  })
+})
+
+
+canvas.addEventListener("mousedown", drawStart);
 canvas.addEventListener("mousemove", drawing);
+canvas.addEventListener("mouseup", drawEnd);
